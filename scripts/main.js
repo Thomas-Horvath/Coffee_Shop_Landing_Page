068abb66f3c9c3e88b2739d0a56a915125f305e2
Thomas-Horvath <thomas.horvath0416@gmail.com>
2024-05-22 17:28:18 +0200

    welcom, hero and favorits section, adding about, history,contact pages

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,32 @@ hamburgerBtn.addEventListener('click', () => {
     hamburgerBtn.classList.toggle("active-hamburger");
 });
 
+menuItems.forEach((menuItem) => {
+    menuItem.addEventListener("click", () => {
+        nav.classList.remove("nav-active");
+    })
+});
+
+
+
+
+
+// const header = document.querySelector(".header");
+// let styles = getComputedStyle(header);
+// console.log(styles.fontFamily);
+
+
+const input = document.querySelector(".search");
+let p = document.querySelector(".banner p");
+input.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e.target.elements.search.value)
+    let text = e.target.elements.search.value;
+    console.log(text.length)
+    if (text.length > 3) {
+
+        p.innerText = text;
+    }
+
+
+});
