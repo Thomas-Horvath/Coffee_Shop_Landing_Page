2ae19aca6fe20bf0433023e0b9ccef2603da603b
Thomas-Horvath <thomas.horvath0416@gmail.com>
2024-06-01 18:37:29 +0200

    adding product images

--- scripts/main.js
+++ scripts/main.js
@@ -8,9 +8,6 @@ const cookiesBtn = document.querySelector('.cookies-btn');
 
 
 
-// hamburger menu function
-hamburgerMenu();
-
 
 
 
@@ -18,41 +15,44 @@ hamburgerMenu();
 // const input = document.querySelector(".search");
 // let p = document.querySelector(".banner p");
 // input.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     console.log(e.target.elements.search.value)
-//     let text = e.target.elements.search.value;
-//     console.log(text.length)
-//     if (text.length > 3) {
-
-//         p.innerText = text;
-//     }
-
-
-// });
-
-
-
-// cookies 
-document.addEventListener("DOMContentLoaded", () => {
-//az oldal betöltődésekor -> lefut a cookiesPopup függvény
-
-    function cookiesPopup() {
-        const acceptedCookies = sessionStorage.getItem("acceptedCookies");
-        // elmentjük a sessionStorege acceptCookies kulcshoz tartozó értéket. (nincs értéke még itt)
-
-        if (!acceptedCookies) { // ha az érték nem true  akkor futnak a függvények
-            setTimeout(() => {
-                cookies.classList.add("cookie-active");
-            }, 3000);
-
-            cookiesBtn.addEventListener("click", () => {
-                cookies.classList.remove("cookie-active");
-                sessionStorage.setItem("acceptedCookies", "true"); // itt true-ra állítjuk az értéket.
+    //     e.preventDefault();
+    //     console.log(e.target.elements.search.value)
+    //     let text = e.target.elements.search.value;
+    //     console.log(text.length)
+    //     if (text.length > 3) {
+        
+        //         p.innerText = text;
+        //     }
+        
+        
+        // });
+        
+        
+        
+        // cookies 
+        document.addEventListener("DOMContentLoaded", () => {
+            //az oldal betöltődésekor -> lefut a cookiesPopup függvény
+            
+            function cookiesPopup() {
+                const acceptedCookies = sessionStorage.getItem("acceptedCookies");
+                // elmentjük a sessionStorege acceptCookies kulcshoz tartozó értéket. (nincs értéke még itt)
+                
+                if (!acceptedCookies) { // ha az érték nem true  akkor futnak a függvények
+                    setTimeout(() => {
+                        cookies.classList.add("cookie-active");
+                    }, 3000);
+                    
+                    cookiesBtn.addEventListener("click", () => {
+                        cookies.classList.remove("cookie-active");
+                        sessionStorage.setItem("acceptedCookies", "true"); // itt true-ra állítjuk az értéket.
             });
         };
     };
-
+    
     cookiesPopup();
 });
 
+
+// hamburger menu function
+hamburgerMenu();
 
